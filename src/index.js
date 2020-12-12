b8d7ace233206dd539d8fc7e30f90e9724d20a19
Deepti Verma <deeptiverma455@gmail.com>
2020-12-12 18:38:37 +0530

    code

--- src/index.js
+++ src/index.js
@@ -7,8 +7,35 @@ const port = 8080
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const { connections } = require('mongoose');
+const {connection} = require('./connector');
 
-
+const isNullOrUndefined = val=> val===null || val===undefined;
+app.get("/findColleges",async(req,res)=>{
+    const name= req.query.name;
+    const state=req.query.state;
+    const city= req.query.city;
+    const course=req.query.course;
+    const exams=req.query.exams;
+    let findClg="";
+    if(!isNullOrUndefined(name)){
+    let regex = new RegExp(name, 'i')
+    findClg= await connection.find({name: {$regex: regex}});
+    }
+    if(!isNullOrUndefined(state)){
+    let regex = new RegExp(state, 'i')
+    findClg= await connection.find({state: {$regex: regex}});
+    }
+    if(!isNullOrUndefined(city)){
+    let regex = new RegExp(city, 'i')
+    findClg= await connection.find({city: {$regex: regex}});
+    }
+    if(!isNullOrUndefined(course)){
+    let regex = new RegExp(course, 'i')
+    findClg= await connection.find({course: {$regex: regex}});
+    }
+    
+    res.send(findClg);
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
